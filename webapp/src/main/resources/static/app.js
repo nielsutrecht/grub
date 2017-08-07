2b4f5df87e7ba050e2a7f43508ab9faed3014000
Niels Dommerholt <ndommerholt@gmail.com>
2017-08-07 17:17:22 +0200

    Added Angular webpack starter

--- webapp/src/main/resources/static/app.js
+++ webapp/src/main/resources/static/app.js
@@ -2,19 +2,18 @@ var appModule = angular.module('myApp', []);
 
 appModule.controller('MainCtrl', ['mainService','$scope','$http',
         function(mainService, $scope, $http) {
-            $scope.greeting = 'Welcome to the JSON Web Token / AngularJR / Spring example!';
             $scope.token = null;
             $scope.error = null;
-            $scope.roleUser = false;
-            $scope.roleAdmin = false;
-            $scope.roleFoo = false;
+            $scope.user = null;
 
             $scope.login = function() {
                 $scope.error = null;
-                mainService.login($scope.userName).then(function(token) {
+
+                mainService.login($scope.email, $scope.password).then(function(token) {
                     $scope.token = token;
                     $http.defaults.headers.common.Authorization = 'Bearer ' + token;
-                    $scope.checkRoles();
+
+                    $scope.me();
                 },
                 function(error){
                     $scope.error = error
@@ -22,16 +21,18 @@ appModule.controller('MainCtrl', ['mainService','$scope','$http',
                 });
             }
 
-            $scope.checkRoles = function() {
-                mainService.hasRole('user').then(function(user) {$scope.roleUser = user});
-                mainService.hasRole('admin').then(function(admin) {$scope.roleAdmin = admin});
-                mainService.hasRole('foo').then(function(foo) {$scope.roleFoo = foo});
-            }
-
             $scope.logout = function() {
                 $scope.userName = '';
+                $scope.password = '';
+
                 $scope.token = null;
                 $http.defaults.headers.common.Authorization = '';
+            }
+
+            $scope.me = function() {
+                mainService.me().then(function(user) {
+                    $scope.user = user;
+                });
             }
 
             $scope.loggedIn = function() {
@@ -43,14 +44,14 @@ appModule.controller('MainCtrl', ['mainService','$scope','$http',
 
 appModule.service('mainService', function($http) {
     return {
-        login : function(username) {
-            return $http.post('/user/login', {name: username}).then(function(response) {
+        login : function(email, password) {
+            return $http.post('/user/login', {email: email, password: password}).then(function(response) {
                 return response.data.token;
             });
         },
 
-        hasRole : function(role) {
-            return $http.get('/api/role/' + role).then(function(response){
+        me : function() {
+            return $http.get('/user/me').then(function(response){
                 console.log(response);
                 return response.data;
             });
